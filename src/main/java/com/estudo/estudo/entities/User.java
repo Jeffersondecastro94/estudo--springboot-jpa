e8bf6719de502f59a8688bb10f9e2943acb33ac5
Jefferson de Castro <Jeffersondecastro94@gmail.com>
2023-01-15 00:57:35 -0300

    configuracao do h2 e Jpa e mapeamento da entidade user com JPA

--- src/main/java/com/estudo/estudo/entities/User.java
+++ src/main/java/com/estudo/estudo/entities/User.java
@@ -3,11 +3,21 @@ package com.estudo.estudo.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
-public class User implements Serializable{
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
-	
+@Entity
+@Table(name="tb_user")
+public class User implements Serializable{
 	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
+	
 	private String name;
 	private String email;
 	private String phone;
@@ -82,6 +92,8 @@ public class User implements Serializable{
 		User other = (User) obj;
 		return Objects.equals(id, other.id);
 	}
+
+
 
 
 	
--- src/main/java/com/estudo/estudo/resources/UserResource.java
+++ src/main/java/com/estudo/estudo/resources/UserResource.java
@@ -1,5 +1,7 @@
 package com.estudo.estudo.resources;
 
+
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
